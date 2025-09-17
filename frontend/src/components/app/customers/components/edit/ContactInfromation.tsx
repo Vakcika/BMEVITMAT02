f94be34a12bd54e172973535cd2f7726afbfb987
Vakci <55985512+Vakcika@users.noreply.github.com>
2025-09-17 14:28:52 +0200

    edit customer input grid fix

--- frontend/src/components/app/customers/components/edit/ContactInfromation.tsx
+++ frontend/src/components/app/customers/components/edit/ContactInfromation.tsx
@@ -13,7 +13,7 @@ export default function ContactInformation({
   return (
     <div className="space-y-4 md:col-span-2">
       <h3 className="text-lg font-medium">Contact Information</h3>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
         <FormInput
           id="name"
           name="name"
