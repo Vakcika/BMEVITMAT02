d2e6f2010e2476e897a4da7d37ee248a50fbb744
Vakci <55985512+Vakcika@users.noreply.github.com>
2025-10-25 19:12:01 +0200

    transaction form grid fix

--- frontend/src/components/app/customers/components/edit/transaction/TransactionFormSection.tsx
+++ frontend/src/components/app/customers/components/edit/transaction/TransactionFormSection.tsx
@@ -27,38 +27,36 @@ export default function TransactionFormSection({
 }: Readonly<TransactionFormSectionProps>) {
   return (
     <div className="space-y-6">
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         <div className="space-y-4">
           {/* Customer Section */}
           <h3 className="text-lg font-medium">Basic Information</h3>
-          <FormSelect
-            label="Customer*"
-            name="customer_id"
-            value={values.customer_id.toString()}
-            onChange={(value) => setFieldValue("customer_id", Number(value))}
-            options={customers}
-            getOptionValue={(c) => c.id.toString()}
-            getOptionLabel={(c) => c.company_name}
-            placeholder="Select customer"
-            emptyLabel="None"
-            error={touched.customer_id && (errors.customer_id as string)}
-          />
-        </div>
-        <div className="space-y-4">
-          {/* Amount Section */}
-          <h3 className="text-lg font-medium">Financial Details</h3>
-          <FormInput
-            id="amount"
-            name="amount"
-            label="Amount*"
-            type="number"
-            step="100"
-            placeholder="0"
-            value={values.amount}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            error={touched.amount && (errors.amount as string)}
-          />
+          <div className="flex flex-wrap gap-4 items-start">
+            <FormSelect
+              label="Customer*"
+              name="customer_id"
+              value={values.customer_id.toString()}
+              onChange={(value) => setFieldValue("customer_id", Number(value))}
+              options={customers}
+              getOptionValue={(c) => c.id.toString()}
+              getOptionLabel={(c) => c.company_name}
+              placeholder="Select customer"
+              emptyLabel="None"
+              error={touched.customer_id && (errors.customer_id as string)}
+            />
+            <FormInput
+              id="amount"
+              name="amount"
+              label="Amount*"
+              type="number"
+              step="100"
+              placeholder="0"
+              value={values.amount}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={touched.amount && (errors.amount as string)}
+            />
+          </div>
         </div>
 
         {/* Notes Section */}
